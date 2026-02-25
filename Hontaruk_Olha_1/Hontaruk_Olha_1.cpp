dc10dd4989478056c127eb8c6258ae7a7d6dc51e
Olha Hontaruk <olha.hontaruk@oa.edu.ua>
2026-02-25 16:49:03 +0200

    Оновлено код першого завдання та додано gitignore

--- Hontaruk_Olha_1/Hontaruk_Olha_1.cpp
+++ Hontaruk_Olha_1/Hontaruk_Olha_1.cpp
@@ -1,20 +1,21 @@
-// Hontaruk_Olha_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
+#include <Windows.h>
+#include <cstdlib>
+using namespace std;
+void fillArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 100;
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+    srand(time(0));
+    
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
