3bf126a060cf77e7f90f8eef0197b8b809d20253
Olha Hontaruk <olha.hontaruk@oa.edu.ua>
2026-02-27 00:01:23 +0200

    Доопрацьовано друге завдання

--- topic_2_task_4/topic_2_task_4.cpp
+++ topic_2_task_4/topic_2_task_4.cpp
@@ -1,20 +1,45 @@
-// topic_2_task_4.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <Windows.h>
+#include <cmath>
+using namespace std;
+
+double factorial(int n)
+{
+    double result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+double productArray(int arr[], int size)
+{
+    double product = 1;
+    for (int i = 0; i < size; i++)
+    {
+        product *= arr[i];
+    }
+    return product;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+    int n = 3;
+    int x[7] = { 5, 6, 7, 3, 9, 8, 4 };
+
+    double num1 = factorial(5 * n - 4) + 8;
+    double den1 = factorial(2 * n + 1);
+    double part1 = num1 / den1;
+
+    double totalP = productArray(x, 7);
+    double num2 = factorial(n) + totalP;
+    double den2 = pow(totalP, 1.0 / 3.0);
+    double part2 = num2 / den2;
+
+    double z = part1 + part2;
+    cout << "Результат обчислення за формулою: Z =" << z << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
